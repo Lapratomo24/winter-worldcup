b15ae975dacea9ebdda92980dac3f33ddf56b24d
Lapratomo24 <lapratomo24@yahoo.com>
2022-04-09 23:06:28 +0000

    create opening title and set up heroku deployment with automatic deploy enabled

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
+import random
+
+from strings import opening_title, closing_remark
+from strings import cyan_colored
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +17,16 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('winter_world_cup')
 
-tournament = SHEET.worksheet('venues')
+venues = SHEET.worksheet('venues')
+groups = SHEET.worksheet('groups')
+fixture_a = SHEET.worksheet('fixture_a')
+fixture_b = SHEET.worksheet('fixture_b')
+fixture_c = SHEET.worksheet('fixture_c')
+fixture_d = SHEET.worksheet('fixture_d')
+fixture_e = SHEET.worksheet('fixture_e')
+fixture_f = SHEET.worksheet('fixture_f')
+fixture_g = SHEET.worksheet('fixture_g')
+fixture_h = SHEET.worksheet('fixture_h')
 
-data = tournament.get_all_values()
-
-print(data)
+opening_title()
+print(cyan_colored("Winter World Cup Is Coming".center(80)))
